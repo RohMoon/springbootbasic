c56b6c1387baff1a8445df2446eb2334892da32b
Moon <rs.moony@gmail.com>
2022-05-10 10:30:29 +0900

    feat: [Spring Boot] add configuration       Application.properties sources
     Application.properties sources and how get priority these properties.

--- src/main/java/com/sm/AppRunner.java
+++ src/main/java/com/sm/AppRunner.java
@@ -1,5 +1,6 @@
 package com.sm;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.annotation.Order;
@@ -8,10 +9,23 @@ import org.springframework.stereotype.Component;
 @Component
 @Order(1)
 public class AppRunner implements ApplicationRunner {
+
+    @Value("${my.name}")
+    String name;
+
+    @Value("${my.age}")
+    int age;
+
+    @Value("${my.fullName}")
+    String fullName;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("AppRunner ApplicationRunner");
         System.out.println("foo : "+ args.containsOption("foo"));
         System.out.println("bar : "+ args.containsOption("bar"));
+        System.out.println(name);
+        System.out.println(age);//-493660209
+        System.out.println(fullName);
     }
 }
---
+++ src/test/java/com/sm/ApplicationTest.java
@@ -0,0 +1,26 @@
+package com.sm;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@TestPropertySource(properties =  "my.age=2" , locations = "classpath:/test.properties")
+@SpringBootTest
+class ApplicationTest {
+
+    @Autowired
+    Environment environment;
+
+    @Test
+    public void contextLoads(){
+        assertThat(environment.getProperty("my.name"))
+                .isEqualTo("ARA");
+    }
+
+}
