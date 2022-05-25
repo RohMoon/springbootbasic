888dd1955547e48640f4e4304a0dabba088681db
Moon <rs.moony@gmail.com>
2022-05-25 17:35:25 +0900

    feat:  [SpringBoot] feat  ViewResolver test sources.
    feat [SpringBoot] ViewResolver  ViewResolver test sources  "userCreate_XML".

--- src/test/java/com/sm/springWebMVCHttpMessageConverters/UserControllerTest.java
+++ src/test/java/com/sm/springWebMVCHttpMessageConverters/UserControllerTest.java
@@ -12,8 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @RunWith(SpringRunner.class)
@@ -36,5 +35,17 @@ public class UserControllerTest{
                 .andExpect(jsonPath("$.username",is(equalTo("sangmoon"))))
                 .andExpect(jsonPath("$.password",is(equalTo("1234"))));
     }
+    @Test// 여기서는 응답은 json으로 보내고 요청은 xml로 받는다.
+    public void createUser_XML() throws Exception{
+        String userJson = "{\"username\": \"sangmoon\", \"password\":\"1234\"}";
 
+        mockMvc.perform(post("/users/create") //요청을 마드는 단계
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_XML)
+                .content(userJson))
+                //응답을 확인하는 단계
+                .andExpect(status().isOk())
+                .andExpect(xpath("/User/username").string("sangmoon"))
+                .andExpect(xpath("/User/password").string("1234"));
+    }
 }
